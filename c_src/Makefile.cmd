47f47c9bda63a55f934cf5362f55710b33f0c2ae
Arash Badie Modiri <arashbm@gmail.com>
2015-06-30 11:31:35 +0430

    resting some mekefile stuff

--- c_src/Makefile.cmd
+++ c_src/Makefile.cmd
@@ -1,10 +1,20 @@
-CMD_DIR= $(dir$(lastword $(MAKEFILE_LIST)))../priv
+CMD_DIR= $(CURDIR)/../priv
 CMD_PATH= $(CMD_DIR)/procket
 
-all: dirs compile
+ERTS_INCLUDE_DIR ?= $(shell erl -noshell -s init stop -eval "io:format(\"~s/erts-~s/include/\", [code:root_dir(), erlang:system_info(version)]).")
+ERL_INTERFACE_INCLUDE_DIR ?= $(shell erl -noshell -s init stop -eval "io:format(\"~s\", [code:lib_dir(erl_interface, include)]).")
+ERL_INTERFACE_LIB_DIR ?= $(shell erl -noshell -s init stop -eval "io:format(\"~s\", [code:lib_dir(erl_interface, lib)]).")
+
+CFLAGS += -fPIC -I $(ERTS_INCLUDE_DIR) -I $(ERL_INTERFACE_INCLUDE_DIR)
+
+all: dirs compile_cmd compile
+
 
 dirs:
 	-@mkdir -p $(CMD_DIR)
 
-compile:
+compile_cmd:
 	$(CC) $(PROCKET_CFLAGS) -g -Wall -o $(CMD_PATH) -L. procket_cmd.c -lancillary
+
+compile:
+	$(CC) $(CFLAGS) -g -Wall -o $(CMD_PATH).so  procket.c -fpic -shared
